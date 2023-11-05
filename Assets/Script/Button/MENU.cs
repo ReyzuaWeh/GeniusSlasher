bb052c83c03a8295d033200bc0c519242f16fdb8
ReyzuaWeh <ginanjar.alfarizi07@gmail.com>
2023-11-05 15:05:28 +0700

    Menu (Done)
    Menu udah beres, paling designnya diperbaiki

---
+++ Assets/Script/Button/MENU.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MENU : MonoBehaviour
+{
+    public void exit()//tombol exit, WAJIB PUBLIC agar dapat diakses
+    {
+        Debug.Log("AKU KELUAR!");
+        Application.Quit();
+    }
+    public void tutorial()
+    {
+        SceneManager.LoadScene("Tutorial");//pindahscene
+    }
+
+    public void lvl1()//lvl1
+    {
+        SceneManager.LoadScene("LVL1");
+    }
+    public void lvl2()//lvl2
+    {
+        SceneManager.LoadScene("LVL2");
+    }
+    public void lvl3()//lvl3
+    {
+        SceneManager.LoadScene("LVL3");
+    }
+}
